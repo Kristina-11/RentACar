691bea974f3443a1d7018953c4578103b5810346
Kristina <tina91kris11@gmail.com>
2021-03-02 19:00:20 +0100

    Added React Router.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Navbar from "./components/shared/Navbar";
+
 function App() {
   return (
     <div className="App">
-      
+      <div className="columns">
+        <div className="column">
+          <Navbar />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/shared/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <nav className="navbar is-dark">
+            <div className="container nav">
+                <div className="nav-left is-size-1-widescreen">
+                    <h1>Rent A Car</h1>
+                </div>
+                <div className="nav-right is-size-4">
+                    <a href="">One</a>
+                    <a href="">Two</a>
+                    <a href="">Three</a>
+                </div>
+            </div>
+        </nav>
+    )
+}
